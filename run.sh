539ccae8706c3113cda8f7324e21131e481030a0
lty <lty4246@foxmail.com>
2024-11-05 16:35:42 +0800

    yunxing jiaoben

---
+++ run.sh
@@ -0,0 +1,10 @@
+##!/bin/bash
+#
+## 激活 Conda 环境
+#source /home/lty/anaconda3/bin/activate myenv
+
+# 运行 Python 脚本
+python readProj_tif.py /home/lty/data/SEU/2024-11-4.tif
+
+# 如果需要在完成后停留在该环境下，可以取消以下注释
+# exec "$SHELL"
